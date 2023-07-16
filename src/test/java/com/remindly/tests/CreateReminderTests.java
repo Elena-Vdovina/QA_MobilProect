24324faa5c06fd6b77c00d7b3cb3b675975b8c87
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-07-16 15:31:32 +0200

    Homework 14.07

---
+++ src/test/java/com/remindly/tests/CreateReminderTests.java
@@ -0,0 +1,43 @@
+package com.remindly.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateReminderTests extends TestBase {
+
+  @Test
+  public void addReminderWithDefaultData() {
+
+    int quantityBeforeAdd = app.getMainScreen().getTotalReminders();
+    //tap on add reminder
+    app.getMainScreen().tapOnAddReminder();
+    //fill reminder title
+    app.getReminder().enterTitle("Test");
+    //save reminder
+    app.getReminder().saveReminder();
+    int quantityAfterAdd = app.getMainScreen().getTotalReminders();
+    //assert to add new reminder
+    Assert.assertEquals(quantityAfterAdd, quantityBeforeAdd + 1);
+
+  }
+
+  @Test
+  public void addReminderPositiveTest() {
+    //tap on add reminder
+    app.getMainScreen().tapOnAddReminder();
+    //fill reminder title
+    app.getReminder().enterTitle("1 September");
+    //select date
+    app.getReminder().selectDate("future", "SEP", 3, 0, "future", "2024");
+    //select time
+    app.getReminder().selectTime("am", 405, 690, 542, 1191);
+    //enter repetition interval id=RepeatNo
+    app.getReminder().addReminderInterval("5");
+    //select type of repetition id=RepeatType
+    app.getReminder().getTypeOfRepetition("Day");
+    //save reminder
+    app.getReminder().saveReminder();
+  }
+
+  //  xpath = //*[@text='" + typeRep + "']
+}
